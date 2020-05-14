2949b2e6d84a19e59765d5664a44004d89891792
Jake G <jake098877@gmail.com>
2020-05-14 17:56:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,8 +17,9 @@ class Artist
 
   def add_song_by_name(song_name)
     song_name = Song.new(song_name)
-    @songs << song_name
+
     song_name.artist = self
+      @songs << song_name
   end
 
   def self.song_count
